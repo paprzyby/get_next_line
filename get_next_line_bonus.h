37088692b590969b65b678507dca460b0e2ec1da
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-09-09 15:48:29 +0200

    added the BUFFER_SIZE to .h file

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -13,6 +13,10 @@
 #if !defined(GET_NEXT_LINE_BONUS_H)
 # define GET_NEXT_LINE_BONUS_H
 
+# if !defined(BUFFER_SIZE)
+#  define BUFFER_SIZE 5
+# endif
+
 # include <stdlib.h>
 # include <stddef.h>
 # include <unistd.h>
